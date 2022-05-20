2737fcf340b019ed2e39bba2e3ab49cf8e99370d
Vincent Trélat <vincent.trelat@free.fr>
2022-05-20 11:19:51 +0200

    Update RankAnnotatedTree.thy

--- RankAnnotatedTree.thy
+++ RankAnnotatedTree.thy
@@ -26,10 +26,13 @@ fun rbst :: "('a::linorder) rtree \<Rightarrow> bool" where
 value "rbst \<langle>\<langle>\<langle>\<langle>\<rangle>, 0, 3, \<langle>\<rangle>\<rangle> , 1, 4, \<langle>\<langle>\<rangle>, 0, 5, \<langle>\<rangle>\<rangle>\<rangle>, 3, 6::nat, \<langle>\<langle>\<langle>\<rangle>, 0, 7,\<langle>\<rangle>\<rangle>, 1, 8, \<langle>\<langle>\<rangle>, 0, 9,\<langle>\<rangle>\<rangle>\<rangle>\<rangle>"
 value "rbst \<langle>\<langle>\<langle>\<langle>\<rangle>, 0, 3, \<langle>\<rangle>\<rangle> , 2, 4, \<langle>\<langle>\<rangle>, 0, 5, \<langle>\<rangle>\<rangle>\<rangle>, 3, 6::nat, \<langle>\<langle>\<langle>\<rangle>, 0, 7,\<langle>\<rangle>\<rangle>, 1, 8, \<langle>\<langle>\<rangle>, 0, 9,\<langle>\<rangle>\<rangle>\<rangle>\<rangle>"
 
+lemma set_rtree_rbst: "rbst \<langle>l, n, x, r\<rangle> \<Longrightarrow> a \<in> set_rtree \<langle>l, n, x, r\<rangle> \<Longrightarrow> a < x \<Longrightarrow> a \<in> set_rtree l"
+  by force
+
 fun rins :: "'a::linorder \<Rightarrow> 'a rtree \<Rightarrow> 'a rtree" where
 "rins a \<langle>\<rangle> = \<langle>\<langle>\<rangle>, 0, a, \<langle>\<rangle>\<rangle>" |
-"rins a \<langle>l, n, x, r\<rangle> = (if a < x then \<langle>rins a l, n + 1, x, r\<rangle>
-                      else if a = x then \<langle>l, n, x, r\<rangle>
+"rins a \<langle>l, n, x, r\<rangle> = (if a \<in> set_rtree \<langle>l, n, x, r\<rangle> then \<langle>l, n, x, r\<rangle>
+                      else if a < x then \<langle>rins a l, n + 1, x, r\<rangle>
                       else \<langle>l, n, x, rins a r\<rangle>)"
 
 value "rins 9 \<langle>\<langle>\<langle>\<langle>\<rangle>, 0, 3, \<langle>\<rangle>\<rangle> , 1, 4, \<langle>\<langle>\<rangle>, 0, 5, \<langle>\<rangle>\<rangle>\<rangle>, 3, 6::nat, \<langle>\<langle>\<langle>\<rangle>, 0, 7,\<langle>\<rangle>\<rangle>, 1, 8, \<langle>\<rangle>\<rangle>\<rangle>"
@@ -37,8 +40,11 @@ value "rins 9 \<langle>\<langle>\<langle>\<langle>\<rangle>, 0, 3, \<langle>\<ra
 lemma rins_set[simp]: "set_rtree (rins x t) = insert x (set_rtree t)"
   by (induction t arbitrary: x rule: set_rtree.induct) auto
 
-lemma num_nodes_rins[simp]: "x \<notin> set_rtree t \<Longrightarrow> rbst t \<Longrightarrow> num_nodes (rins x t) = 1 + num_nodes t"
+lemma num_nodes_rins_notin[simp]: "x \<notin> set_rtree t \<Longrightarrow> rbst t \<Longrightarrow> num_nodes (rins x t) = 1 + num_nodes t"
   by (induction t rule: rbst.induct) simp+
+
+lemma num_nodes_rins_in[simp]: "x \<in> set_rtree t \<Longrightarrow> rbst t \<Longrightarrow> num_nodes (rins x t) = num_nodes t"
+  by (induction t rule: rbst.induct) auto
 
 lemma rins_invar[simp]: "x \<notin> set_rtree t \<Longrightarrow> rbst t \<Longrightarrow> rbst (rins x t)"
 proof (induction t rule: rbst.induct)
@@ -50,6 +56,10 @@ next
     by auto
 qed
 
+lemma rins_invar_in[simp]: "x \<in> set_rtree t \<Longrightarrow> rbst t \<Longrightarrow> rbst (rins x t)"
+  using rbst.elims(2) by force
+
+
 subsection \<open>Inorder traversal and getting rank\<close>
 
 fun inorder:: "'a rtree \<Rightarrow> 'a list" where
@@ -60,7 +70,9 @@ value  "inorder \<langle>\<langle>\<langle>\<langle>\<rangle>, 0, 3, \<langle>\<
 
 fun rank:: "'a::linorder \<Rightarrow> 'a rtree \<Rightarrow> nat" where
 "rank a \<langle>\<rangle> = 0" |
-"rank a \<langle>l, n, x, r\<rangle> = (if a > x then 1 + num_nodes l + rank a r
+"rank a \<langle>l, n, x, r\<rangle> = (if a \<notin> set_rtree \<langle>l, n, x, r\<rangle> then undefined
+                       else if a = x then n
+                       else if a > x then 1 + n + rank a r
                        else rank a l)"
 
 value  "rank 9 \<langle>\<langle>\<langle>\<langle>\<rangle>, 0, 3, \<langle>\<rangle>\<rangle> , 1, 4, \<langle>\<langle>\<rangle>, 0, 5, \<langle>\<rangle>\<rangle>\<rangle>, 3, 6::nat, \<langle>\<langle>\<langle>\<rangle>, 0, 7,\<langle>\<rangle>\<rangle>, 1, 8, \<langle>\<langle>\<rangle>, 0, 9,\<langle>\<rangle>\<rangle>\<rangle>\<rangle>"
@@ -70,15 +82,52 @@ definition "at_index i l x \<equiv> i < length l \<and> l!i=x"
 lemma num_nodes_inorder[simp]: "num_nodes t = length (inorder t)"
   by (induction t) simp+
 
+lemma set_rtree_inorder_in[simp]:"x \<in> set_rtree t \<longleftrightarrow> x \<in> set(inorder t)"
+  by (induction t) auto
+
 lemma inorder_index: "rbst t \<Longrightarrow> a \<in> set_rtree t \<Longrightarrow> at_index (rank a t) (inorder t) a"
 proof (induction t)
   case Leaf
   then show ?case by simp
 next
   case (Node l n x r)
-  then show ?case sorry
-qed
+  then show ?case
+  proof (cases "a = x")
+    case True
+    hence "rank a \<langle>l, n, x, r\<rangle> = n"
+      by simp
+    have "n = num_nodes l"
+      using Node.prems(1) rbst.simps(2) by blast
+    then have "... = length (inorder l)"
+      by simp
+    then show ?thesis using True by (auto split: if_splits simp add: \<open>n = num_nodes l\<close> at_index_def)
+  next
+    case False
+    then show ?thesis
+    proof (cases "a > x")
+      case True
+      hence "rank a \<langle>l, n, x, r\<rangle> = 1 + n + rank a r"
+        using Node.prems(2) by auto
+      from True have "a \<in> set_rtree r"
+        using Node.prems(1) Node.prems(2) order_less_imp_not_less by fastforce
+      then show ?thesis using True
+        apply (auto split: if_splits simp add: at_index_def)
+         apply (metis Node.IH(2) Node.prems(1) \<open>a \<in> set_rtree r\<close> at_index_def nat_add_left_cancel_less num_nodes_inorder rbst.simps(2))
+        by (metis (no_types, lifting) Node.IH(2) Node.prems(1) \<open>a \<in> RankAnnotatedTree.set_rtree r\<close> add_Suc_right add_diff_cancel_left' at_index_def not_add_less1 nth_Cons_Suc nth_append num_nodes_inorder rbst.simps(2))
+    next
+      case False
+      hence "rank a \<langle>l, n, x, r\<rangle> = rank a l" using \<open>a \<noteq> x\<close>
+        using Node.prems(2) by auto
+      from False \<open>a \<noteq>x\<close> have "a \<in> set_rtree l"
+        by (meson Node.prems(1) Node.prems(2) antisym_conv3 set_rtree_rbst)
+      then show ?thesis using False \<open>a \<noteq> x\<close>
+        apply (auto split: if_splits simp add: at_index_def)
+         apply (metis Node.IH(1) Node.prems(1) \<open>a \<in> RankAnnotatedTree.set_rtree l\<close> at_index_def less_Suc_eq rbst.simps(2) trans_less_add1)
+        by (metis Node.IH(1) Node.prems(1) \<open>a \<in> RankAnnotatedTree.set_rtree l\<close> at_index_def nth_append rbst.simps(2))
+    qed
+  qed
 
+qed
 
 subsection \<open>Selection in a rank annotated tree\<close>
 
@@ -90,7 +139,7 @@ fun sel:: "nat \<Rightarrow> 'a::linorder rtree \<Rightarrow>'a" where
 
 value  "sel 5 \<langle>\<langle>\<langle>\<langle>\<rangle>, 0, 3, \<langle>\<rangle>\<rangle> , 1, 4, \<langle>\<langle>\<rangle>, 0, 5, \<langle>\<rangle>\<rangle>\<rangle>, 3, 6::nat, \<langle>\<langle>\<langle>\<rangle>, 0, 7,\<langle>\<rangle>\<rangle>, 1, 8, \<langle>\<langle>\<rangle>, 0, 9,\<langle>\<rangle>\<rangle>\<rangle>\<rangle>" 
 
-lemma select_correct: "rbst t \<Longrightarrow> i < length (inorder t) \<Longrightarrow> select i t = inorder t!i"
+lemma select_correct: "rbst t \<Longrightarrow> i < length (inorder t) \<Longrightarrow> select i t = inorder t!i" sorry
 
 
 end
